6dcfd724c6b31b1dcafc71153381a2b9d05383df
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2024-09-07 22:23:12 +0330

    use cors in app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import express from 'express';
-import "dotenv/config"
-import hotelsRoute from './routes/hotels-route';
-const app =express();
+import cors from "cors";
+import express from "express";
+import "dotenv/config";
+import hotelsRoute from "./routes/hotels-route";
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use("/api/hotels", hotelsRoute);
 
-app.use("/api/hotels",hotelsRoute)
-
-
-const APP_PORT = process.env.NODE_ENV === 'production'? process.env.PORT : 3000;
+const APP_PORT =
+  process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
 app.listen(APP_PORT, () => {
-    console.log("app in running on port " + APP_PORT + " (env: " + process.env.NODE_ENV);
-})
+  console.log(
+    "app in running on port " + APP_PORT + " (env: " + process.env.NODE_ENV
+  );
+});
